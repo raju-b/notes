7ce39e44d2bfc7696be49303f354451a9a983e64
nagaraju-exalogic <nagaraju@exalogic.in>
2019-08-22 16:19:37 +0530

    notes update

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,8 +1,6 @@
 class Note < ApplicationRecord
 	belongs_to :user,required: false
-	validates :title, presence: true
-  validates :body, presence: true
-  validates :tag,presence: true   
+	validates :title, presence:{message: "please enter book name"}
   def self.search(search)
 	  	if search
 				where("title ILIKE ?","%#{search}%")
